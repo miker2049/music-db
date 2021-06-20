a4c81729463d2c3b7c79f3cd63c4ae2803fed200
Mike Russo <6748794-prolegomenatoy1@users.noreply.gitlab.com>
2021-06-20 17:20:00 -0400

    whew

---
+++ index.js
@@ -0,0 +1,232 @@
+
+// require('dotenv').config()
+const path = require('path')
+const fs = require('fs')
+const { spawn } = require('child_process')
+const sqlite3 = require('sqlite3').verbose();
+
+
+
+const FEATURE_CONFIG_PATH = "featureConfig";
+const AUDIO_FILE_CONFIG_PATH = "sounds/testcsus4.wav";
+const FEATURE_ARGS = ['run',
+  '--rm=true',
+  `--volume=${__dirname}:/wd`,
+  '--workdir=/wd',
+  'yaafe',
+  '-r', '16000',
+  '--verbose',
+  '--dump-dataflow=df',
+  '--resample',
+  '-c', FEATURE_CONFIG_PATH,
+  AUDIO_FILE_CONFIG_PATH];
+const FRAMES_ARGS = ['run',
+  '--rm=true',
+  `--volume=${__dirname}:/wd`,
+  '--workdir=/wd',
+  'yaafe',
+  '-r', '44100',
+  '--verbose',
+  '--dump-dataflow=df',
+  '--resample',
+  "-f", "frames: Frames blockSize=1024 stepSize=1024",
+  AUDIO_FILE_CONFIG_PATH];
+
+
+function getAubioOnsets() {
+  let onsets = ''
+  return new Promise((res, rej) => {
+    const aubio_process = spawn('aubioonset', [AUDIO_FILE_CONFIG_PATH]);
+
+    aubio_process.stdout.on('data', (data) => {
+      console.log(`stdout: ${data}`);
+      onsets += data;
+    });
+
+    aubio_process.stderr.on('data', (data) => {
+      console.error(`stderr: ${data}`);
+    });
+
+    aubio_process.on('close', (code) => {
+      console.log(`child process exited with code ${code}`);
+      if (code > 0) {
+        rej()
+      } else {
+        res(
+          onsets.split('\n')
+            .filter(e => e ? true : false)
+            .map(i => parseFloat(i))
+        )
+      }
+    });
+
+  })
+}
+
+function get_expected_csv_names(path, sound) {
+  let arr = [];
+  const text = fs.readFileSync(path).toString()
+  for (let line of text.split('\n')) {
+    const name = line.match(/^(\w+)\:/)
+    // console.log(name)
+    if (name) {
+      arr.push(`${sound}.${name[1]}.csv`)
+
+    }
+  }
+  return arr
+}
+function run_yaafe(args) {
+  return new Promise((res, rej) => {
+    const yaafe_process = spawn('docker', args);
+
+    yaafe_process.stdout.on('data', (data) => {
+      console.log(`stdout: ${data}`);
+    });
+
+    yaafe_process.stderr.on('data', (data) => {
+      console.error(`stderr: ${data}`);
+    });
+
+    yaafe_process.on('close', (code) => {
+      console.log(`child process exited with code ${code}`);
+      if (code > 0) {
+        rej()
+      } else {
+        res()
+      }
+    });
+
+  })
+}
+
+function getFeatureKeys(files) {
+  let keys = []
+  files.forEach(item => {
+
+    const match = item.match(/wav\.(.+)\.csv/)
+    keys.push(match[1])
+  });
+  return keys
+}
+
+// https://stackoverflow.com/a/47764403
+function checkExistsWithTimeout(filePath, timeout) {
+  return new Promise(function(resolve, reject) {
+
+    var timer = setTimeout(function() {
+      watcher.close();
+      reject(new Error('File did not exists and was not created during the timeout.'));
+    }, timeout);
+
+    fs.access(filePath, fs.constants.R_OK, function(err) {
+      if (!err) {
+        clearTimeout(timer);
+        watcher.close();
+        resolve(filePath);
+      }
+    });
+
+    var dir = path.dirname(filePath);
+    var basename = path.basename(filePath);
+    var watcher = fs.watch(dir, function(eventType, filename) {
+      if (eventType === 'rename' && filename === basename) {
+        clearTimeout(timer);
+        watcher.close();
+        resolve(filePath);
+      }
+    });
+  });
+}
+
+async function rm_if_exists(filepath) {
+  try {
+    await fs.promises.stat(filepath)
+  } catch (err) {
+    return true
+  }
+  await fs.promises.rm(filepath)
+  return true
+}
+async function cleanCSV(files) {
+  await Promise.all(files.map((item, i, arr) => rm_if_exists(item)))
+}
+
+async function create_csv_files() {
+  //clean
+  const files = get_expected_csv_names(FEATURE_CONFIG_PATH, AUDIO_FILE_CONFIG_PATH)
+  await cleanCSV(files)
+  await run_yaafe(FEATURE_ARGS)
+  await run_yaafe(FRAMES_ARGS)
+  const assuredfiles = await Promise.all(files.map((item, i, arr) => checkExistsWithTimeout(item, 100)))
+  return assuredfiles
+}
+
+
+async function parse_csv_file(file) {
+  const lines = (await fs.promises.readFile(file)).toString().split('\n')
+  let meta = [], data = []
+  lines.forEach(line => {
+    if (line[0] === '%') {
+      line.substring(2).split(' ').forEach(item => {
+        meta.push(
+          item.split('=')
+        )
+      });
+    } else {
+      data.push(line)
+    }
+  })
+  return [meta, data]
+}
+
+const dumpers = {
+  "Loudness": (meta, data, db) => {
+
+  }
+}
+
+async function add_file_to_db(file, table, db) {
+  const [meta, data] = await parse_csv_file(file)
+  db.serialize(function() {
+    let metaobj = {}
+    meta.forEach(v => metaobj[v[0]] = v[1])
+    // const table=meta.find((v)=>v[0]==='yaafedefinition')[1].split(' ')[0]
+    const table = metaobj.yaafedefinition
+    // console.log(metaobj.blockSize)
+    // console.log(metaobj.stepSize)
+    // console.log(metaobj.samplerate)
+    // console.log(path.basename(file))
+    // const json = JSON. stringify(Object.assign(data, { test: 'harm' }))
+    // console.dir(json)
+    // db.run(`INSERT INTO fileindex (filename, amplitudeModulation) VALUES("testyydasy", json('${json}'));`)
+  })
+}
+// run_yaafe().then(t=>console. log('done!'))
+
+// db.serialize(function() {
+//   db.run("CREATE TABLE lorem (info TEXT)");
+
+//   const stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+//   for (var i = 0; i < 10; i++) {
+//       stmt.run("Ipsum " + i);
+//   }
+//   stmt.finalize();
+
+//   db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
+//       console.log(row.id + ": " + row.info);
+//   });
+// });
+(async function() {
+  const db = new sqlite3.Database('./DB.db', sqlite3.OPEN_READWRITE);
+  const files = await create_csv_files().catch((err) => { console.log(err) })
+  const onsets = await getAubioOnsets()
+  getFeatureKeys(files)
+  for (let file of files) {
+    add_file_to_db(file, 'test', db)
+  }
+  // console.log(files)
+  // await add_file_to_db(files[0], db)
+  db.close()
+})()
+// db.close();
---
+++ schema.sql
@@ -0,0 +1,312 @@
+CREATE TABLE IF NOT EXISTS "Loudness" (
+"id" INTEGER,
+"filename"	TEXT,
+"normalize"	INTEGER,
+"resample"	INTEGER,
+"samplerate"	INTEGER,
+"blockSize"	INTEGER,
+"stepSize"	INTEGER,
+"rowNumber" INTEGER,
+"bark1" REAL,
+"bark2" REAL,
+"bark3" REAL,
+"bark4" REAL,
+"bark5" REAL,
+"bark6" REAL,
+"bark7" REAL,
+"bark8" REAL,
+"bark9" REAL,
+"bark10" REAL,
+"bark11" REAL,
+"bark12" REAL,
+"bark13" REAL,
+"bark14" REAL,
+"bark15" REAL,
+"bark16" REAL,
+"bark17" REAL,
+"bark18" REAL,
+"bark19" REAL,
+"bark20" REAL,
+"bark21" REAL,
+"bark22" REAL,
+"bark23" REAL,
+"bark24" REAL,
+PRIMARY KEY("id" AUTOINCREMENT)
+);
+
+CREATE TABLE IF NOT EXISTS "Frames" (
+"id"	INTEGER NOT NULL,
+"filename"	TEXT,
+"normalize"	INTEGER,
+"resample"	INTEGER,
+"samplerate"	INTEGER,
+"blockSize"	INTEGER,
+"stepSize"	INTEGER,
+"rowNumber" INTEGER,
+"data"	BLOB,
+PRIMARY KEY("id" AUTOINCREMENT)
+);
+
+
+CREATE TABLE IF NOT EXISTS "Chroma" (
+"id"	INTEGER NOT NULL,
+"filename"	TEXT,
+"normalize"	INTEGER,
+"resample"	INTEGER,
+"samplerate"	INTEGER,
+"blockSize"	INTEGER,
+"stepSize"	INTEGER,
+"rowNumber" INTEGER,
+"c"	REAL,
+"c#" REAL,
+"d" REAL,
+"d#" REAL,
+"e" REAL,
+"f" REAL,
+"f#" REAL,
+"g" REAL,
+"g#" REAL,
+"a" REAL,
+"a#" REAL,
+"b" REAL,
+PRIMARY KEY("id" AUTOINCREMENT)
+);
+
+CREATE TABLE IF NOT EXISTS "Chroma2" (
+"id"	INTEGER NOT NULL,
+"filename"	TEXT,
+"normalize"	INTEGER,
+"resample"	INTEGER,
+"samplerate"	INTEGER,
+"blockSize"	INTEGER,
+"stepSize"	INTEGER,
+"rowNumber" INTEGER,
+"c"	REAL,
+"c#" REAL,
+"d" REAL,
+"d#" REAL,
+"e" REAL,
+"f" REAL,
+"f#" REAL,
+"g" REAL,
+"g#" REAL,
+"a" REAL,
+"a#" REAL,
+"b" REAL,
+PRIMARY KEY("id" AUTOINCREMENT)
+);
+
+
+CREATE TABLE IF NOT EXISTS "LPC" (
+"id"	INTEGER NOT NULL,
+"filename"	TEXT,
+"normalize"	INTEGER,
+"resample"	INTEGER,
+"samplerate"	INTEGER,
+"blockSize"	INTEGER,
+"stepSize"	INTEGER,
+"rowNumber" INTEGER,
+"LPCNbCoeffs0" REAL,
+"LPCNbCoeffs1" REAL,
+"LPCNbCoeffs2" REAL,
+PRIMARY KEY("id" AUTOINCREMENT)
+);
+
+
+CREATE TABLE IF NOT EXISTS "MFCC" (
+"id" INTEGER,
+"filename"	TEXT,
+"normalize"	INTEGER,
+"resample"	INTEGER,
+"samplerate"	INTEGER,
+"blockSize"	INTEGER,
+"stepSize"	INTEGER,
+"rowNumber" INTEGER,
+"CepsNbCoeff1" REAL,
+"CepsNbCoeff2" REAL,
+"CepsNbCoeff3" REAL,
+"CepsNbCoeff4" REAL,
+"CepsNbCoeff5" REAL,
+"CepsNbCoeff6" REAL,
+"CepsNbCoeff7" REAL,
+"CepsNbCoeff8" REAL,
+"CepsNbCoeff9" REAL,
+"CepsNbCoeff10" REAL,
+"CepsNbCoeff11" REAL,
+"CepsNbCoeff12" REAL,
+"CepsNbCoeff13" REAL,
+PRIMARY KEY("id" AUTOINCREMENT)
+);
+
+
+CREATE TABLE IF NOT EXISTS "SpectralFlatness" (
+"id"	INTEGER NOT NULL,
+"filename"	TEXT,
+"normalize"	INTEGER,
+"resample"	INTEGER,
+"samplerate"	INTEGER,
+"blockSize"	INTEGER,
+"stepSize"	INTEGER,
+"rowNumber" INTEGER,
+"sf" REAL,
+PRIMARY KEY("id" AUTOINCREMENT)
+);
+
+CREATE TABLE IF NOT EXISTS "SpectralDecrease" (
+"id"	INTEGER NOT NULL,
+"filename"	TEXT,
+"normalize"	INTEGER,
+"resample"	INTEGER,
+"samplerate"	INTEGER,
+"blockSize"	INTEGER,
+"stepSize"	INTEGER,
+"rowNumber" INTEGER,
+"sd" REAL,
+PRIMARY KEY("id" AUTOINCREMENT)
+);
+
+
+CREATE TABLE IF NOT EXISTS "SpectralShapeStatistics" (
+"id"	INTEGER NOT NULL,
+"filename"	TEXT,
+"normalize"	INTEGER,
+"resample"	INTEGER,
+"samplerate"	INTEGER,
+"blockSize"	INTEGER,
+"stepSize"	INTEGER,
+"rowNumber" INTEGER,
+"centroid" REAL,
+"spread" REAL,
+"skewness" REAL,
+"kurtosis" REAL,
+PRIMARY KEY("id" AUTOINCREMENT)
+);
+
+CREATE TABLE IF NOT EXISTS "SpectralSlope" (
+"id"	INTEGER NOT NULL,
+"filename"	TEXT,
+"normalize"	INTEGER,
+"resample"	INTEGER,
+"samplerate"	INTEGER,
+"blockSize"	INTEGER,
+"stepSize"	INTEGER,
+"rowNumber" INTEGER,
+"ss" REAL,
+PRIMARY KEY("id" AUTOINCREMENT)
+);
+
+CREATE TABLE IF NOT EXISTS "SpectralVariation" (
+"id"	INTEGER NOT NULL,
+"filename"	TEXT,
+"normalize"	INTEGER,
+"resample"	INTEGER,
+"samplerate"	INTEGER,
+"blockSize"	INTEGER,
+"stepSize"	INTEGER,
+"rowNumber" INTEGER,
+"sv" REAL,
+PRIMARY KEY("id" AUTOINCREMENT)
+);
+
+CREATE TABLE IF NOT EXISTS "PerceptualSpread" (
+"id"	INTEGER NOT NULL,
+"filename"	TEXT,
+"normalize"	INTEGER,
+"resample"	INTEGER,
+"samplerate"	INTEGER,
+"blockSize"	INTEGER,
+"stepSize"	INTEGER,
+"rowNumber" INTEGER,
+"ps" REAL,
+PRIMARY KEY("id" AUTOINCREMENT)
+);
+
+
+CREATE TABLE IF NOT EXISTS "PerceptualSharpness" (
+"id"	INTEGER NOT NULL,
+"filename"	TEXT,
+"normalize"	INTEGER,
+"resample"	INTEGER,
+"samplerate"	INTEGER,
+"blockSize"	INTEGER,
+"stepSize"	INTEGER,
+"rowNumber" INTEGER,
+"psh" REAL,
+PRIMARY KEY("id" AUTOINCREMENT)
+);
+
+CREATE TABLE IF NOT EXISTS "ComplexDomainOnsetDetection" (
+"id"	INTEGER NOT NULL,
+"filename"	TEXT,
+"normalize"	INTEGER,
+"resample"	INTEGER,
+"samplerate"	INTEGER,
+"blockSize"	INTEGER,
+"stepSize"	INTEGER,
+"rowNumber" INTEGER,
+"onset" REAL,
+PRIMARY KEY("id" AUTOINCREMENT)
+);
+
+
+CREATE TABLE IF NOT EXISTS "OBSI" (
+"id"	INTEGER NOT NULL,
+"filename"	TEXT,
+"normalize"	INTEGER,
+"resample"	INTEGER,
+"samplerate"	INTEGER,
+"blockSize"	INTEGER,
+"stepSize"	INTEGER,
+"rowNumber" INTEGER,
+"OBSI1" REAL,
+"OBSI2" REAL,
+"OBSI3" REAL,
+"OBSI4" REAL,
+"OBSI5" REAL,
+"OBSI6" REAL,
+"OBSI7" REAL,
+"OBSI8" REAL,
+"OBSI9" REAL,
+PRIMARY KEY("id" AUTOINCREMENT)
+);
+
+
+CREATE TABLE IF NOT EXISTS "OBSIR" (
+"id"	INTEGER NOT NULL,
+"filename"	TEXT,
+"normalize"	INTEGER,
+"resample"	INTEGER,
+"samplerate"	INTEGER,
+"blockSize"	INTEGER,
+"stepSize"	INTEGER,
+"rowNumber" INTEGER,
+"OBSIR1" REAL,
+"OBSIR2" REAL,
+"OBSIR3" REAL,
+"OBSIR4" REAL,
+"OBSIR5" REAL,
+"OBSIR6" REAL,
+"OBSIR7" REAL,
+"OBSIR8" REAL,
+PRIMARY KEY("id" AUTOINCREMENT)
+);
+
+CREATE TABLE IF NOT EXISTS "AmplitudeModulation" (
+"id"	INTEGER NOT NULL,
+"filename"	TEXT,
+"normalize"	INTEGER,
+"resample"	INTEGER,
+"samplerate"	INTEGER,
+"blockSize"	INTEGER,
+"stepSize"	INTEGER,
+"rowNumber" INTEGER,
+"AM1" REAL,
+"AM2" REAL,
+"AM3" REAL,
+"AM4" REAL,
+"AM5" REAL,
+"AM6" REAL,
+"AM7" REAL,
+"AM8" REAL,
+PRIMARY KEY("id" AUTOINCREMENT)
+);
